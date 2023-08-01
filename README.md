153b4e97209e2018e050756e764b55bd627cf03a
ryo.kanayama <k221t043@st.kyoto-seika.ac.jp>
2023-08-01 15:31:23 +0900

    送信完了

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # seika-2q-cc
+## 見出し2
+
+```
+ npm run build
+```
--- src/app.js
+++ src/app.js
@@ -2,12 +2,12 @@ import Chart from 'chart.js/auto';
 
 (async function() {
   const data = [
-    { year: "ゲーム性", count: 18 },
-    { year: "グラフィック", count: 20 },
-    { year: "世界観", count: 25 },
-    { year: "コンテンツ", count: 20 },
-    { year: "BGM", count: 21 },
-    { year: "個人的好み", count: 28 },
+    { year: "かわいい", count: 27 },
+    { year: "おもしろい", count: 26 },
+    { year: "やんちゃ", count: 27 },
+    { year: "人見知り", count: 29 },
+    { year: "頭の良さ", count: 28 },
+    { year: "大きさ", count: 26 },
   ];
 
   new Chart(
@@ -18,20 +18,20 @@ import Chart from 'chart.js/auto';
         labels: data.map(row => row.year),
         datasets: [
           {
-            label: 'MH4',
+            label: 'ログ',
             data: data.map(row => row.count),
             borderColor: 'rgba(116, 80, 48, 0.6)',
             backgroundColor: 'rgba(116, 80, 48, 0.2)',
           },
           {
-            label: 'MHWI',
+            label: 'ショコラ',
             data: [26, 30, 28, 25, 27, 29], 
             borderColor: 'rgba(169, 206, 236, 1)',
             backgroundColor: 'rgba(169, 206, 236, 0.2)',
           },
           {
-            label: 'MHRS',
-            data: [21, 17, 22, 26, 20, 26], 
+            label: 'ネロ',
+            data: [29, 28, 27, 28, 29, 28], 
             borderColor: 'rgba(255, 255, 0, 1)',
             backgroundColor: 'rgba(255, 255, 0, 0.2)',
           }
